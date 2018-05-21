df9fe3a8245f603041f0adf070a233d27dd7318d
Montag55 <temeter@yahoo.com>
2018-05-21 16:39:40 +0200

    3.2 done

--- InfoVis_Assignment_SS15/src/infovis/paracoords/MouseController.java
+++ InfoVis_Assignment_SS15/src/infovis/paracoords/MouseController.java
@@ -1,17 +1,22 @@
 package infovis.paracoords;
 
+import infovis.scatterplot.Data;
 import infovis.scatterplot.Model;
 
-import java.awt.Shape;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.Iterator;
 
 public class MouseController implements MouseListener, MouseMotionListener {
 	private View view = null;
 	private Model model = null;
 	Shape currentShape = null;
-	
+	private int x_click;
+	private int y_click;
+
+
 	public void mouseClicked(MouseEvent e) {
 		
 	}
@@ -21,19 +26,29 @@ public class MouseController implements MouseListener, MouseMotionListener {
 	}
 
 	public void mouseExited(MouseEvent e) {
-
+		
 	}
 
 	public void mousePressed(MouseEvent e) {
+		this.x_click = e.getX();
+		this.y_click = e.getY();
+		Iterator<Data> iter = model.iterator();
 
+		while(iter.hasNext()){
+			iter.next().setColor(Color.BLACK);
+		}
+
+		view.repaint();
 	}
 
 	public void mouseReleased(MouseEvent e) {
-
+		view.getMarkerRectangle().setRect(0,0,0,0);
+		view.repaint();
 	}
 
 	public void mouseDragged(MouseEvent e) {
-
+		view.getMarkerRectangle().setRect(this.x_click, this.y_click, e.getX() - this.x_click, e.getY() - this.y_click);
+		view.repaint();
 	}
 
 	public void mouseMoved(MouseEvent e) {
--- InfoVis_Assignment_SS15/src/infovis/paracoords/View.java
+++ InfoVis_Assignment_SS15/src/infovis/paracoords/View.java
@@ -29,8 +29,8 @@ public class View extends JPanel {
 		drawRaster(g2D);
 		drawLabels(g2D);
 		drawData(g2D);
-		//g2D.setColor(Color.BLACK);
-		//g2D.draw(getMarkerRectangle());
+		g2D.setColor(Color.BLACK);
+		g2D.draw(getMarkerRectangle());
 	}
 
 	@Override
@@ -62,10 +62,10 @@ public class View extends JPanel {
 	public void drawData(Graphics2D g2D){
 
 		double line_height = getHeight() - 40;
-		g2D.setColor(Color.DARK_GRAY);
 
 		for(int e = 0; e < model.getList().size(); e++) {
 
+			g2D.setColor(model.getList().get(e).getColor());
 			double min = model.getRanges().get(0).getMin();
 			double max = model.getRanges().get(0).getMax();
 			double val_off = (max - model.getList().get(e).getValue(0))/(max - min);
@@ -81,40 +81,13 @@ public class View extends JPanel {
 				Point2D tmp = new Point2D.Double(i*(getWidth() - 20)/model.getDim() + 20, line_height * val_off + 20);
 				Line2D line = new Line2D.Double(old, tmp);
 
+				if(line.intersects(getMarkerRectangle()))
+					model.getList().get(e).setColor(Color.RED);
+
 				g2D.draw(line);
 
 				old = tmp;
 			}
 		}
 	}
-
-
-		/*
-		for(int i = 0; i < model.getDim(); i++){
-
-			double x = (i+2)*getHeight()*0.95/model.getDim();
-			double y = (i+0.4)*getHeight()*0.95/model.getDim();
-			double box_size = getHeight()*0.95/model.getDim()-2;
-
-			for(int e = 0; e < model.getList().size(); e++) {
-
-				double min_1 = model.getRanges().get(i).getMin();
-				double max_1 = model.getRanges().get(i).getMax();
-				double val_1_off = (max_1 - model.getList().get(e).getValue(i))/(max_1 - min_1);
-
-				double min_2 = model.getRanges().get(j).getMin();
-				double max_2 = model.getRanges().get(j).getMax();
-				double val_2_off = (max_2 - model.getList().get(e).getValue(j))/(max_2 - min_2);
-
-				Rectangle2D rect = new Rectangle2D.Double(x + (box_size - 3) * val_1_off, y + (box_size - 3) * val_2_off, 3, 3);
-
-				if(getMarkerRectangle().contains(rect))
-					model.getList().get(e).setColor(Color.RED);
-
-				g2D.setColor(model.getList().get(e).getColor());
-				g2D.draw(rect);
-			}
-		}
-	}
-	*/
 }
